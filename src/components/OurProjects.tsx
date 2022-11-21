39325aa5b8129e8d4951014dfd44b3a4edca1125
Felipe F <feferreira.sf@gmail.com>
2022-11-21 18:51:02 -0300

    feat: added link to Projects page

--- src/components/OurProjects.tsx
+++ src/components/OurProjects.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
+import { FiChevronRight } from 'react-icons/fi'
 
 import proj01 from '../assets/project-01.png'
 import proj02 from '../assets/project-02.png'
@@ -76,6 +79,14 @@ export function OurProjects() {
             <p>Delex Relay</p>
           </div>
         </div>
+      </div>
+      <div className='flex justify-center'>
+        <Link
+          href={'/projects'}
+          className='w-60 mt-14 py-4 border-2 rounded flex items-center justify-center gap-2 hover:bg-white hover:text-black duration-300 ease-in-out'>
+          Explore More Projects
+          <FiChevronRight size={24} />
+        </Link>
       </div>
     </section>
   )
