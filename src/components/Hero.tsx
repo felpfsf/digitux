0cc08e4b3d20a14494d11275163501bf8da1705e
Felipe F <feferreira.sf@gmail.com>
2022-11-21 13:15:23 -0300

    feat: hero section mobile and desktop

---
+++ src/components/Hero.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { FiArrowRight, FiMessageSquare } from 'react-icons/fi'
+
+import arrow from '../assets/hero__arrow.png'
+import heroBanner from '../assets/hero__banner.png'
+
+export function Hero() {
+  return (
+    <section className='w-full pt-16 px-4'>
+      <div className='w-full flex flex-col xl:flex-row items-center justify-between'>
+        <div className='relative max-w-[523px] w-full'>
+          <Image
+            src={arrow}
+            quality={100}
+            alt={''}
+            className='hidden 2xl:block h-[432px] absolute top-4 -left-28'
+          />
+          <p className='font-bold xl:text-[22px]'>Hello,</p>
+          <h1 className='text-[40px] xl:text-[48px] font-bold mt-4 leading-[80px]'>
+            We Help People To Bring Their Ideas Alive
+          </h1>
+          <p className='text-lg xl:text-[22px] text-white/80 font-light leading-relaxed mt-8'>
+            A talented team to help you in your journey on creating usefull and
+            easy to use product
+          </p>
+
+          <div className='mt-6 flex flex-col xl:flex-row items-center gap-8'>
+            <a
+              href=''
+              className='w-full text-sm font-semibold py-4 bg-digitux-primary rounded flex-1 flex items-center justify-center gap-2'>
+              Let&apos;s Talk
+              <FiMessageSquare size={24} />
+            </a>
+            <Link
+              href={'/services'}
+              className='flex items-center gap-2 hover:underline underline-offset-4'>
+              Check our Services
+              <FiArrowRight className='' />
+            </Link>
+          </div>
+        </div>
+
+        <Image
+          src={heroBanner}
+          alt={''}
+          quality={100}
+          className='hidden xl:block'
+        />
+      </div>
+    </section>
+  )
+}
