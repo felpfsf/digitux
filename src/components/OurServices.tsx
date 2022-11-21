21516aca5d7ba30fc95315cb9ba5d37c3165ed20
Felipe F <feferreira.sf@gmail.com>
2022-11-21 16:13:17 -0300

    feat: added our services section to home page

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,7 +1,34 @@
-export function OurServices(){
-  return(
-    <div>
-      <h2>The Services we offer</h2>
-    </div>
+import Link from 'next/link'
+
+import data from '../data/dataMockup.json'
+
+import { FiChevronRight } from 'react-icons/fi'
+
+export function OurServices() {
+  return (
+    <section className='w-full mt-32 px-4'>
+      <h2 className='xl:text-[22px] font-semibold leading-relaxed'>
+        The Services we offer
+      </h2>
+      <div className='flex flex-wrap sm:items-center sm:justify-center xl:grid grid-cols-4 grid-rows-2 gap-10'>
+        <h1 className='mt-4 text-[22px] xl:text-[40px] font-bold leading-9 xl:leading-normal col-span-2'>
+          We offer a variaty of services to help you to grow and build your
+          brand and help you with developing your Products
+        </h1>
+
+        {data.services.map(({ id, title, description }) => (
+          <div key={id} className='w-[240px] mt-12 flex flex-col gap-[14px]'>
+            <h1 className='text-xl font-bold'>{title}</h1>
+            <p className='text-white/80'>{description}</p>
+            <Link
+              href={'/services'}
+              className='w-40 py-4 border-2 rounded flex items-center justify-center gap-2 hover:bg-white hover:text-black duration-300 ease-in-out'>
+              Learn More
+              <FiChevronRight size={24} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 
 import { Hero } from '../components/Hero'
 import { Clients } from '../components/Clients'
+import { OurServices } from '../components/OurServices'
 
 export default function Home() {
   return (
@@ -11,7 +12,8 @@ export default function Home() {
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
       className='max-w-[1440px] w-full mx-auto'>
       <Hero />
-      <Clients/>
+      <Clients />
+      <OurServices />
     </motion.section>
   )
 }
