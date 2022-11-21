b1a5880938a5e12cc5685750d64a407c03965b9d
Felipe F <feferreira.sf@gmail.com>
2022-11-21 16:44:15 -0300

    feat: added favIcon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel='icon' type='image/png' href='/dUX_logo.png' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
           rel='preconnect'
