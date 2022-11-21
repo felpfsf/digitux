510e3eaaec13bc3ca50ff1ec8e054460866c2a16
Felipe F <feferreira.sf@gmail.com>
2022-11-21 16:02:12 -0300

    feat: added client section to home

---
+++ src/components/Clients.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+
+import logoMicrosoft from '../assets/client_microsoft.png'
+import logoGoogle from '../assets/client_google.png'
+import logoSpotify from '../assets/client_spotify.png'
+import logoTesla from '../assets/client_tesla.png'
+import logoYassir from '../assets/client_yassir.png'
+import logoTikTok from '../assets/client_tiktok.png'
+
+export function Clients() {
+  return (
+    <section className='w-full mt-32 px-4'>
+      <h1 className='text-2xl font-bold'>Client We helped</h1>
+      <div className='mt-7 xl:mt-12 overflow-x-auto scrollbar-hide flex items-center xl:justify-evenly gap-12'>
+        <Image
+          src={logoMicrosoft}
+          alt={'microsoft company logo'}
+          className='w-32 h-7 xl:h-8'
+        />
+        <Image
+          src={logoGoogle}
+          alt={'Google company logo'}
+          className='w-32 h-7 xl:h-10'
+        />
+        <Image
+          src={logoSpotify}
+          alt={'Spotify company logo'}
+          className='w-32 h-7 xl:h-10'
+        />
+        <Image
+          src={logoTesla}
+          alt={'Tesla company logo'}
+          className='w-32 h-7 xl:h-10'
+        />
+        <Image
+          src={logoYassir}
+          alt={'Yassir company logo'}
+          className='w-32 h-7 xl:h-10'
+        />
+        <Image
+          src={logoTikTok}
+          alt={'TikTok company logo'}
+          className='w-32 h-7 xl:h-12'
+        />
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-
 import { motion } from 'framer-motion'
+
 import { Hero } from '../components/Hero'
+import { Clients } from '../components/Clients'
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
       className='max-w-[1440px] w-full mx-auto'>
       <Hero />
+      <Clients/>
     </motion.section>
   )
 }
