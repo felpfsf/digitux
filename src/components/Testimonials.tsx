eaf60528061ef70be67927f27f9ad1fd914a7f99
Felipe F <feferreira.sf@gmail.com>
2022-11-21 19:20:46 -0300

    style: section header size on desktop

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,7 +2,7 @@ export function Tesimonials() {
   return (
     <section className='w-full mt-32 px-4'>
       <h2 className='xl:text-[22px] font-semibold'>Testimonial</h2>
-      <h1 className='mt-4 text-[22px] xl:text-[34px] font-bold'>
+      <h1 className='mt-4 text-[22px] xl:text-[34px] font-bold max-w-[730px]'>
         You still hesitate about working with Us ? Check what They say about Us
       </h1>
       {/* To-do: testimonial slider */}
