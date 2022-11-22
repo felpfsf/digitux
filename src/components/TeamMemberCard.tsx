d85ef9da1c0cf7a2d0d50163af7f232b433a9f62
Felipe F <feferreira.sf@gmail.com>
2022-11-22 20:14:22 -0300

    feat: TeamMember component card dynamic rendering

---
+++ src/components/TeamMemberCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image'
+
+import tikTik from '../assets/Titik-titik.png'
+
+interface ITeamMemberCardProps {
+  imgUrl: string
+  imgAlt: string
+  name: string
+  position: string
+}
+
+export const TeamMemberCard = ({
+  imgAlt,
+  imgUrl,
+  name,
+  position
+}: ITeamMemberCardProps) => {
+  return (
+    <>
+      <div className='relative max-w-[350px] mt-7 xl:mr-20 flex flex-col justify-center gap-4'>
+        <Image
+          className='z-[9] w-[350px] h-[353px] object-cover'
+          src={`/${imgUrl}`}
+          alt={imgAlt}
+          width={350}
+          height={353}
+          quality={100}
+        />
+        <Image
+          className='absolute -top-6 -right-24 hidden xl:block'
+          src={tikTik}
+          alt={''}
+        />
+        <div className='flex flex-col gap-4'>
+          <h2 className='text-lg xl:text-[22px] font-bold'>{name}</h2>
+          <p className='xl:text-lg text-white/80'>{position}</p>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -1,13 +1,24 @@
 import Image from 'next/image'
 import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
-
 import teamMemberJamila from '../assets/team-member-jamila.png'
 import tikTik from '../assets/Titik-titik.png'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import {
+  SwiperButton,
+  SwiperButtonNext,
+  SwiperButtonPrev
+} from './SwiperButton'
+
+import { TeamMemberCard } from './TeamMemberCard'
+
+import data from '../data/dataMockup.json'
+
 export function TeamSection() {
   return (
-    <section className='w-full mt-32 px-4 flex flex-col lg:flex-row items-center lg:justify-between xl:gap-48'>
-      <div className='max-w-[540px] xl:mb-20'>
+    <section className='w-full mt-32 px-4 flex flex-wrap items-center lg:justify-between gap-7 xl:gap-48'>
+      <div className='max-w-[540px] xl:mb-20 md:flex-1'>
         <h2 className='xl:text-[22px] font-bold leading-relaxed'>
           The Team behind DigitUX
         </h2>
@@ -19,32 +30,14 @@ export function TeamSection() {
           your idea to the next level and help you with your product
         </p>
       </div>
-      <div className='relative max-w-[350px] mt-7 xl:mr-20 flex flex-col justify-center gap-4'>
-        <Image
-          className='z-[9]'
-          src={teamMemberJamila}
-          alt={
-            'A white woaman with auburn hair using a notebook computer looking to you'
-          }
-        />
-        <Image
-          className='absolute -top-6 -right-24 hidden xl:block'
-          src={tikTik}
-          alt={''}
-        />
-        <div>
-          <h2>Jamila Smail</h2>
-          <p>Co-Founder</p>
-        </div>
-        <div className='mt-6 self-center xl:self-end flex gap-12'>
-          <span className='w-12 h-12 rounded-full bg-digitux-primary flex items-center justify-center'>
-            <FiArrowLeft size={20} />
-          </span>
-          <span className='w-12 h-12 rounded-full bg-digitux-primary flex items-center justify-center'>
-            <FiArrowRight size={20} />
-          </span>
-        </div>
-      </div>
+      <Swiper className='flex-1'>
+        {data.teamMembers.map(props => (
+          <SwiperSlide key={props.id} className='flex justify-center gap-9'>
+            <TeamMemberCard {...props} />
+          </SwiperSlide>
+        ))}
+        <SwiperButton />
+      </Swiper>
     </section>
   )
 }
