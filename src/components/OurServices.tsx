4d42b9493d545d023a184c281914855ac4ef8e84
Felipe F <feferreira.sf@gmail.com>
2022-11-22 18:31:31 -0300

    refactor: ServiceCard component dynamic rendering

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link'
+import { ServiceCard } from './ServiceCard'
 
 import data from '../data/dataMockup.json'
-
-import { FiChevronRight } from 'react-icons/fi'
 
 export function OurServices() {
   return (
@@ -15,18 +13,8 @@ export function OurServices() {
           We offer a variaty of services to help you to grow and build your
           brand and help you with developing your Products
         </h1>
-
-        {data.services.map(({ id, title, description }) => (
-          <div key={id} className='w-[240px] mt-12 flex flex-col gap-[14px]'>
-            <h1 className='text-xl font-bold'>{title}</h1>
-            <p className='text-white/80'>{description}</p>
-            <Link
-              href={'/services'}
-              className='w-40 py-4 border-2 rounded flex items-center justify-center gap-2 hover:bg-white hover:text-black duration-300 ease-in-out'>
-              Learn More
-              <FiChevronRight size={24} />
-            </Link>
-          </div>
+        {data.services.map(props => (
+          <ServiceCard key={props.id} {...props} />
         ))}
       </div>
     </section>
