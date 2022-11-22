0c6e6163e5313e70a52ac8173850ffd17ab2d8ae
Felipe F <feferreira.sf@gmail.com>
2022-11-22 17:41:25 -0300

    style: image width and heigh

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -19,7 +19,14 @@ export function TestimonialCard({
       <div className='max-w-md mt-14 flex flex-col gap-8'>
         <p className='text-white/60'>{testimonial}</p>
         <div className='flex items-center gap-2'>
-          <Image src={`/${picUrl}`} alt='' width={69} height={69} quality={100} className='rounded-full'/>
+          <Image
+            src={`/${picUrl}`}
+            alt=''
+            width={70}
+            height={70}
+            quality={100}
+            className='w-[70px] h-[70px] rounded-full'
+          />
           <div>
             <h2 className='text-lg font-semibold'>{name}</h2>
             <p className='text-white/80'>{role}</p>
