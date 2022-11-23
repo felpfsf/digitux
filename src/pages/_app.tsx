e321bfb43e48799cf9a0cd7d9a40e7ad3bd7bf27
Felipe F <feferreira.sf@gmail.com>
2022-11-23 11:33:57 -0300

    feat: page title meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Footer } from '../components/Footer'
 import { Navbar } from '../components/Navbar'
 import '../styles/global.css'
@@ -6,6 +7,9 @@ import '../styles/global.css'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>DigitUX</title>
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
