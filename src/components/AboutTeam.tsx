050f4cc823c8884295c4f1261d4d40340a2a39c3
Felipe F <feferreira.sf@gmail.com>
2022-11-23 15:05:06 -0300

    feat: About page desktop and mobile

--- src/components/AboutTeam.tsx
+++ src/components/AboutTeam.tsx
@@ -1,17 +1,31 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
+import { SwiperButton } from './SwiperButton'
+
+import Image from 'next/image'
+
+import teamMember01 from '../assets/team-member-01.jpg'
+
+import { TeamMemberCard } from './TeamMemberCard'
+import data from '../data/dataMockup.json'
 
 export function AboutTeam() {
   return (
-    <div className='w-full mt-32 px-4'>
-      <div className='flex flex-col gap-4'>
+    <div className='w-full mt-32 px-4 xl:flex items-center'>
+      <div className='flex flex-col mx-auto gap-4 max-w-[350px]'>
         <h2 className='text-[22px] font-bold'>The team</h2>
-        <h1 className='text-4xl text-[40px] font-bold leading-[60px]'>
-          Meet The Awesome and Creative members of Our Team{' '}
+        <h1 className='text-4xl xl:text-[40px] font-bold leading-[60px] sm:text-center xl:text-left'>
+          Meet The Awesome and Creative members of Our Team
         </h1>
       </div>
-      <Swiper>
-        <SwiperSlide></SwiperSlide>
+
+      <Swiper slidesPerView={2} className='mt-20'>
+        {data.teamMembers.map(props => (
+          <SwiperSlide key={props.id} className='ml-2'>
+            <TeamMemberCard {...props} />
+          </SwiperSlide>
+        ))}
+        <SwiperButton />
       </Swiper>
     </div>
   )
