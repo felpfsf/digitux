55dbf843af262a2be456ea8414fef109d07d8125
Felipe F <feferreira.sf@gmail.com>
2022-11-23 12:49:13 -0300

    feat: About page and Hero section

---
+++ src/components/AboutHero.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { FiArrowRight, FiMessageSquare } from 'react-icons/fi'
+
+export function AboutHero() {
+  return (
+    <div className='max-w-3xl w-full mx-auto pt-28 px-4 sm:flex flex-col justify-center items-center'>
+      <h2 className='xl:text-[22px] font-bold'>About Us</h2>
+      <h1 className='mt-4 text-4xl xl:text-[40px] font-bold sm:text-center'>
+        What we do is what make us Who we are
+      </h1>
+      <p className='mt-8 text-lg text-white/80 sm:text-center'>
+        DigitUx is a Digital agencay founded 2022, that create User Centred
+        Product that help her client to evolve ,our core idealogy is Perfection
+        , our goal is to make usefull and easy to use products And help people
+        to grow their business,
+      </p>
+      <div className='w-full sm:w-3/4 mt-11 flex flex-col sm:flex-row items-center justify-center gap-8'>
+        <Link
+          href=''
+          className='w-full text-sm font-semibold py-4 bg-digitux-primary rounded flex flex-1 items-center justify-center gap-2 hover:filter hover:contrast-200 transition-color ease-in-out duration-300'>
+          Let&apos;s Talk
+          <FiMessageSquare size={24} />
+        </Link>
+        <Link
+          href={'/services'}
+          className='text-lg sm:text-[22px] flex flex-grow items-center gap-2 hover:underline underline-offset-8'>
+          Check our Services
+          <FiArrowRight />
+        </Link>
+      </div>
+    </div>
+  )
+}
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 
 import { motion } from 'framer-motion'
+import { AboutTeam } from '../../components/AboutTeam'
+import { AboutHero } from '../../components/AboutHero'
+import { Tesimonials } from '../../components/Testimonials'
 
 export default function About() {
   return (
@@ -8,15 +11,18 @@ export default function About() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
-      className='max-w-[1154px] w-full h-screen mx-auto flex flex-col items-center justify-center '>
-      <h1 className='text-8xl font-extrabold'>About</h1>
+      className='max-w-[1440px] w-full mx-auto'>
+      <AboutHero />
+      <AboutTeam />
+      <Tesimonials />
+      {/* <h1 className='text-8xl font-extrabold'>About</h1>
       <h2 className='text-4xl my-4'>Quem somos</h2>
       <p className='text-2xl'>Este é um teste de rotas</p>
       <Link
         className='text-2xl underline underline-offset-4 text-purple-500'
         href='/'>
         Retornar
-      </Link>
+      </Link> */}
     </motion.section>
   )
 }
