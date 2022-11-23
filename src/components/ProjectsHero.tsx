89917b37c767baec28ba50cfd7613b68f8f36c7c
Felipe F <feferreira.sf@gmail.com>
2022-11-23 18:31:42 -0300

    feat: Projects Hero section mobile and desktop

--- src/components/ProjectsHero.tsx
+++ src/components/ProjectsHero.tsx
@@ -2,28 +2,48 @@ import Link from 'next/link'
 import { FiArrowRight, FiMessageSquare } from 'react-icons/fi'
 
 export function ProjectsHero() {
+  const stats = [
+    { id: 1, title: 'Happy Clients', number: '150' },
+    { id: 2, title: 'Project Done', number: '450' },
+    { id: 3, title: 'Solutions Created', number: '10' },
+    { id: 4, title: 'Bussiness Growed', number: '25' }
+  ]
   return (
     <section className='max-w-3xl w-full mx-auto pt-28 px-4 sm:flex flex-col items-center justify-center'>
       <h2 className='xl:text-[22px] font-bold'>Our Projects</h2>
       <h1 className='mt-4 text-4xl xl:text-[40px] font-bold sm:text-center'>
-      Project We Realised
+        Project We Realised
       </h1>
       <p className='mt-8 text-lg xl:text-[22px] text-white/60 sm:text-center leading-relaxed'>
-      Our team helped alot of startup and company to realise their Products and helped thousnd of client to grow their business
+        Our team helped alot of startup and company to realise their Products
+        and helped thousnd of client to grow their business
       </p>
-      <div className='w-full sm:w-3/4 mt-11 flex flex-col sm:flex-row items-center justify-center gap-8'>
-        <Link
-          href={'/contact'}
-          className='w-full text-sm font-semibold py-4 bg-digitux-primary rounded flex flex-1 items-center justify-center gap-2 hover:filter hover:contrast-200 transition-color ease-in-out duration-300'>
-          Let&apos;s Talk
-          <FiMessageSquare size={24} />
-        </Link>
-        <Link
-          href={'/services'}
-          className='text-lg sm:text-[22px] flex flex-grow items-center gap-2 hover:underline underline-offset-8'>
-          Check our Services
-          <FiArrowRight />
-        </Link>
+      <div className='flex flex-col md:flex-col-reverse items-center'>
+        <div className='w-full sm:w-3/4 mt-11 flex flex-col items-center justify-center gap-8'>
+          <Link
+            href={'/contact'}
+            className='w-full sm:w-1/2 text-sm font-semibold py-4 bg-digitux-primary rounded flex items-center justify-center gap-2 hover:filter hover:contrast-200 transition-color ease-in-out duration-300'>
+            Let&apos;s Talk
+            <FiMessageSquare size={24} />
+          </Link>
+          <Link
+            href={''}
+            className='xl:hidden text-lg sm:text-[22px] flex items-center gap-2 hover:underline underline-offset-8'>
+            Check our Project
+            <FiArrowRight />
+          </Link>
+        </div>
+        <div className='mt-24 md:mt-11 grid grid-cols-2 sm:grid-flow-col gap-12 sm:gap-[70px]'>
+          {stats.map(({ id, title, number }) => (
+            <div key={id}>
+              <h2 className='font-semibold'>{title}</h2>
+              <h1 className='mt-2 text-4xl font-bold'>
+                <span>+</span>
+                {number}
+              </h1>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import { ProjectsHero } from '../../components/ProjectsHero'
+import { Tesimonials } from '../../components/Testimonials'
 
 export default function Projects() {
   return (
@@ -9,6 +10,7 @@ export default function Projects() {
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
       className='max-w-[1440px] w-full mx-auto'>
       <ProjectsHero />
+      <Tesimonials />
     </motion.section>
   )
 }
