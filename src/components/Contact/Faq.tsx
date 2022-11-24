fcfc1f42a3f90d6ca92cacbc0e0ebb12715b0c06
Felipe F <feferreira.sf@gmail.com>
2022-11-24 20:16:55 -0300

    style: adjusting lineheight

--- src/components/Contact/Faq.tsx
+++ src/components/Contact/Faq.tsx
@@ -14,7 +14,7 @@ export function Faq() {
     <section className='w-full px-4 mt-32 mx-auto flex flex-col lg:flex-row items-center'>
       <div className='max-w-[480px] mx-auto'>
         <h2 className='text-[22px] font-bold'>FAQ</h2>
-        <h1 className='text-[40px] font-bold leading-relaxed'>
+        <h1 className='text-[40px] font-bold leading-snug md:leading-relaxed'>
           Here are Some Answer For Frequntly asked Questions
         </h1>
         <p className='text-[22px] text-white/60 mt-10'>
