f1e9019f608f0e43581fbb968f8acf6193c699b1
Felipe F <feferreira.sf@gmail.com>
2022-11-24 19:01:09 -0300

    feat: Faq session

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,7 @@
 import { motion } from 'framer-motion'
 import { ContactHero } from '../../components/Contact/ContactHero'
+import { Faq } from '../../components/Contact/Faq'
+import { OurServices } from '../../components/OurServices/OurServices'
 
 export default function Contact() {
   return (
@@ -8,7 +10,9 @@ export default function Contact() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
       className='max-w-[1440px] w-full mx-auto'>
-        <ContactHero/>        
-      </motion.main>
+      <ContactHero />
+      <Faq />
+      <OurServices />
+    </motion.main>
   )
 }
