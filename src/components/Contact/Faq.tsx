73aa9de2ee67f1f939f90920cfe31514d145702f
Felipe F <feferreira.sf@gmail.com>
2022-11-24 19:01:50 -0300

    feat: Faq session mobile and desktop

---
+++ src/components/Contact/Faq.tsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from 'react'
+import { Accordion } from '../AccordionFaq/Accordion'
+
+export function Faq() {
+  const [clicked, setClicked] = useState(false)
+
+  const contentEl = useRef<HTMLDivElement>(null)
+
+  const handleToggle = () => {
+    setClicked(prev => !prev)
+  }
+
+  return (
+    <section className='w-full px-4 mt-32 mx-auto flex flex-col lg:flex-row items-center'>
+      <div className='max-w-[480px] mx-auto'>
+        <h2 className='text-[22px] font-bold'>FAQ</h2>
+        <h1 className='text-[40px] font-bold leading-relaxed'>
+          Here are Some Answer For Frequntly asked Questions
+        </h1>
+        <p className='text-[22px] text-white/60 mt-10'>
+          We got alot of Message With the same Questions so here are some fast
+          answer for populare Question
+        </p>
+      </div>
+      {/* Accordion */}
+      <div className='max-w-[540px] w-full h-[492px] mt-20 mx-auto'>
+        <Accordion />
+      </div>
+    </section>
+  )
+}
