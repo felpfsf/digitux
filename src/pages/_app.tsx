40d496ed8bcc55519270f993a17618bb56f8d111
Felipe F <feferreira.sf@gmail.com>
2022-11-24 19:58:44 -0300

    feat: adjusting meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='keywords'
+          content='ui, ux, design, webdev, webdevelopment, nextjs'
+        />
         <title>DigitUX</title>
       </Head>
       <Navbar />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta charSet='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta
-          name='keywords'
-          content='ui, ux, design, webdev, webdevelopment, nextjs'
-        />
         <link rel='icon' type='image/png' href='/dUX_logo.png' />
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link
