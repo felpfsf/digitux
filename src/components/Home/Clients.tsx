22ea1787102b59e36ebef738d7efc846ef60f67b
Felipe F <feferreira.sf@gmail.com>
2022-11-24 19:59:26 -0300

    refactor: new clients logo

--- src/components/Home/Clients.tsx
+++ src/components/Home/Clients.tsx
@@ -1,47 +1,27 @@
 import Image from 'next/image'
 
-import logoMicrosoft from '../../assets/client_microsoft.png'
-import logoGoogle from '../../assets/client_google.png'
-import logoSpotify from '../../assets/client_spotify.png'
-import logoTesla from '../../assets/client_tesla.png'
-import logoYassir from '../../assets/client_yassir.png'
-import logoTikTok from '../../assets/client_tiktok.png'
+import logoMs from '../../assets/logo-microsoft-4096.png'
+import logoGg from '../../assets/logo-google-4096.png'
+import logoSp from '../../assets/logo-spotify-4096.png'
+import logoTl from '../../assets/logo-tesla-1024.png'
+import logoYh from '../../assets/logo-yahoo-4096.png'
+import logoCa from '../../assets/logo-codecademy-4096.png'
+import logoGp from '../../assets/logo-gympass-4096.png'
+import logoSr from '../../assets/logo-spiral-4096.png'
 
 export function Clients() {
   return (
     <section className='w-full mt-32 px-4'>
       <h1 className='text-2xl font-bold'>Client We helped</h1>
       <div className='mt-7 xl:mt-12 overflow-x-auto scrollbar-hide flex items-center xl:justify-evenly gap-12'>
-        <Image
-          src={logoMicrosoft}
-          alt={'microsoft company logo'}
-          className='w-32 h-7 xl:h-8 xl:w-auto'
-        />
-        <Image
-          src={logoGoogle}
-          alt={'Google company logo'}
-          className='w-32 h-7 xl:h-10'
-        />
-        <Image
-          src={logoSpotify}
-          alt={'Spotify company logo'}
-          className='w-32 h-7 xl:h-10 xl:w-auto'
-        />
-        <Image
-          src={logoTesla}
-          alt={'Tesla company logo'}
-          className='w-32 h-7 xl:h-10'
-        />
-        <Image
-          src={logoYassir}
-          alt={'Yassir company logo'}
-          className='w-32 h-7 xl:h-10'
-        />
-        <Image
-          src={logoTikTok}
-          alt={'TikTok company logo'}
-          className='w-32 h-7 xl:h-12'
-        />
+        <Image src={logoMs} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoGg} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoSp} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoTl} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoYh} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoCa} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoGp} alt={''} className='w-32 h-full object-cover' />
+        <Image src={logoSr} alt={''} className='w-32 h-full object-cover' />
       </div>
     </section>
   )
